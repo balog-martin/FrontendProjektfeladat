7181c18bdee4b56d625f2a56a1a63484b7fa1ef3
balog-martin <balogmartin92@gmail.com>
2024-02-06 01:00:09 +0100

    Footer pozicionalasa: vmivel jobb, de meg mindig fura....

--- scriptStyle.js
+++ scriptStyle.js
@@ -8,11 +8,11 @@ function adjustFooterMargin()
 	let footer = document.getElementById("idFooter")
     let factor = 17.46;
 
-    let result = width / factor;
-    //footerText.style.left = result.toString() + 'px';
+    //let result = width / factor;
+    let result = 0.02252 * width + 35.48;
 
-    footer.style.left = result.toString() + '%'
-
+    footer.style.left = result.toString() + '%';
+    footer.style.transform = "translate(-" + (result).toString() + "%,-50%)";
 }
 
 adjustFooterMargin();
